d4d5be126f06130db2468cfb0c8ead64f58e425b
Safia <safia.ibrahim2121@gmail.com>
2022-03-31 17:08:36 +0100

    complete

--- sql/products/create-products.sql
+++ sql/products/create-products.sql
@@ -1,8 +1,17 @@
-CREATE TABLE products(
+CREATE TABLE products( 
  id serial primary key,
  name varchar(255) not null,
  price int not null,
  discount boolean,
  unique(name)
 )
+
+CREATE TABLE films (
+  id   SERIAL PRIMARY KEY,
+  title         TEXT NOT NULL,
+  score            INT,
+  year_released    INT,
+  genre            TEXT
+);
+
 
--- sql/products/insert-products.sql
+++ sql/products/insert-products.sql
@@ -2,3 +2,26 @@ insert into products(name, price, discount) VALUES ('apple', 135, false);
 insert into products(name, price, discount) VALUES ('orange', 55, true);
 insert into products(name, price, discount) VALUES ('kiwi', 165, true);
 insert into products(name, price, discount) VALUES ('banana', 035, false);
+
+insert into films
+(title, genre, year_reliesed, score) VALUES ('Shawshank Redemption', 'Drama', 1991, 9);
+
+insert into film(title, genre,year_released,score) VALUES ('Godfather', 'Crime', 1972,9);
+
+
+insert into film(title, genre, year_released, score) VALUES ('Dark Knight', 'Action', 2008,9);
+
+
+
+insert into films(title, genre, year_released, score) VALUES ('Alien', 'SciFi', 1979,9);
+insert into films(title, genre, year_released, score) VALUES ('Total Recall', 'SciFi', 1990, 8);
+insert into films(title, genre, year_released, score) VALUES ('Matrix', 'SciFi',1999, 8);
+insert into films(title, genre, year_released, score) VALUES ('Matrix Resurrections', 'SciFi', 2021, 5);
+insert into films(title, genre, year_released, score) VALUES ('Matrix Reloaded', 'SciFi', 2003, 6);
+insert into films(title, genre, year_released, score) VALUES ('Hunt for Red October', 'Thriller',1990, 7);
+insert into films(title, genre, year_released, score) VALUES ('Misery', 'Thriller', 1990, 7);
+insert into films(title, genre, year_released, score) VALUES ('Power Of The Dog', 'Western', 2021, 6);
+insert into films(title, genre, year_released, score) VALUES ('Hell or High Water', 'Western',2016, 8);
+insert into films(title, genre, year_released, score) VALUES ('Good the Bad and the Ugly', ' Western', 1966, 9);
+insert into films(title, genre, year_released, score) VALUES ('Unforgiven', 'Western', 1992, 7);
+
